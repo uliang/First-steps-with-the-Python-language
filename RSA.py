a82872bac7331c73cf0658d0b6023cf65afae10b
uliang <uliang6482@gmail.com>
2017-07-20 21:16:19 +0800

    overflow problems fixed. gcd now computes bezout coefficients and keygen takes N bit length as input

--- RSA.py
+++ RSA.py
@@ -1,6 +1,6 @@
 """
-These two functions are implementations of the RSA encryption decryption 
-protocal created by Rivest, Shamir and Adleman. 
+This script implements the RSA encryption decryption 
+protocal created by Rivest, Shamir and Adleman.  
 
 WARNING: Do not use these scripts in production settings! 
 ------------------------------------------------------------------------
@@ -10,7 +10,8 @@ Email : tang_u_liang@sp.edu.sg
 """ 
 from random import randint
 from sympy import randprime
-from numpy import floor
+from numpy import floor, sqrt
+
 
 def gcd(m, n):
     r = [m,n,m%n]
@@ -21,6 +22,7 @@ def gcd(m, n):
         q = floor(r[0]/r[1])
         s = [s[1], s[2], s[1] - q*s[2]]
         t = [t[1], t[2], t[1] - q*t[2]]
+        
         if r[2] == 0:
             break
         
@@ -34,23 +36,30 @@ def coprimeGen(n):
     """
     Returns a random prime coprime to n.
     """
-    
-    for i in xrange(n):
+    i = 2
+    while i < n:
         p = randprime(i, n)
-        if p%n == 1:
-            return p
+#        print p
+        m, _, _ = gcd(p, n)
+        if m == 1:
             break
-        
+        i += 1
+    return p
 
 def multpInv(x, n):
     """
     Returns x^-1 mod n
     """
-    
+    r, s, _ = gcd(x, n)
+    if r != 1:
+        raise ValueError("{} and {} are not coprime".format(x, n))
+    else:
+        return long(s)
 
 
 def lcm(x, y):
-    return x*y/gcd(x, y)
+    m, _, _ = gcd(x, y)
+    return x*y/m
 
 
 def encrypt(message, public_key):
@@ -63,16 +72,21 @@ def decrypt(c_message, private_key):
     return pow(c_message, d, n)
 
 
-def keygen():
-    N = 10
-    p = randprime(2**(N/2.0), 2**N)
-    q = randprime(2**(N/2.0), 2**N)
+def keygen(N):
+    """
+    Generates cryptographically insecure key pairs with modulus between
+    2N-1 to 2N bits. 
+    """
+    p = randprime(2**(N-1), 2**N)
+    q = randprime(2**(N-1), 2**N)
     n = p*q
     
-    lambda_n = lcm(p-1, q-1)
     
+
+    lambda_n = lcm(p-1, q-1)
+#    print lambda_n
     e = coprimeGen(lambda_n) # public exponent
     
-    d = multpInv(e, lambda_n, coprime) # private exponent
+    d = multpInv(e, lambda_n) # private exponent
     
     return (e, n), (d, n)
