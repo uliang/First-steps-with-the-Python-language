1ed5ba0f1c1ab054dd97803861ee17f9b0e3e65f
uliang <uliang6482@gmail.com>
2017-07-20 19:22:00 +0800

    modified gcd function so that it returns bezout coefficients

--- RSA.py
+++ RSA.py
@@ -10,43 +10,43 @@ Email : tang_u_liang@sp.edu.sg
 """ 
 from random import randint
 from sympy import randprime
-
+from numpy import floor
 
 def gcd(m, n):
     r = [m,n,m%n]
+    s = [None, 1, 0]
+    t = [None, 0, 1]
     
     while True:
+        q = floor(r[0]/r[1])
+        s = [s[1], s[2], s[1] - q*s[2]]
+        t = [t[1], t[2], t[1] - q*t[2]]
         if r[2] == 0:
             break
+        
         r = [r[1], r[2], r[1]%r[2]]
+        
     
-    return r[1]
+    return r[1], s[1], t[1]
 
 
 def coprimeGen(n):
     """
-    Returns a list of numbers coprime to n.
+    Returns a random prime coprime to n.
     """
-    coprimes = list()
-    add_in = coprimes.append
     
-    for i in range(2, n):
-        if gcd(i, n) == 1:
-            add_in(i)
-    
-    return coprimes
+    for i in xrange(n):
+        p = randprime(i, n)
+        if p%n == 1:
+            return p
+            break
+        
 
-
-def multpInv(x, n, mulp_group):
+def multpInv(x, n):
     """
     Returns x^-1 mod n
     """
-    if x not in mulp_group: 
-        raise ValueError("%d is not in multiplicative group" % (x))
-        
-    for y in mulp_group:
-        if (x*y)%n == 1:
-            return y
+    
 
 
 def lcm(x, y):
@@ -71,9 +71,7 @@ def keygen():
     
     lambda_n = lcm(p-1, q-1)
     
-    coprime = coprimeGen(lambda_n)
-    
-    e = coprime[randint(0, len(coprime)-1)] # public exponent
+    e = coprimeGen(lambda_n) # public exponent
     
     d = multpInv(e, lambda_n, coprime) # private exponent
     
