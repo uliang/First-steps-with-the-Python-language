6b33555f92d7e494cc22fc5bdceb2c780bf10aeb
uliang <uliang6482@gmail.com>
2017-07-20 12:34:26 +0800

    Started on RSA algorithm demo

---
+++ RSA.py
@@ -0,0 +1,15 @@
+"""
+These two functions are implementations of the RSA encryption decryption 
+protocal created by Rivest, Shamir and Adleman. 
+
+------------------------------------------------------------------------
+Author : Tang U-Liang 
+Email : tang_u_liang@sp.edu.sg 
+------------------------------------------------------------------------
+""" 
+
+def encrypt(message, public_key):
+    pass 
+
+def decrypt(message, private_key):
+    pass
